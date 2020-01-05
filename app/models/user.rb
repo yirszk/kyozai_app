45e1fa32612135f675957f7beaf68bde27468347
Yoichiro Suzuki <yir.szk@gmail.com>
2020-01-05 15:55:54 +0900

    アカウントの有効化（一部修正）

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token
   before_save :downcase_email
-  before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -22,12 +21,6 @@ class User < ApplicationRecord
     # メールアドレスを全て小文字にする
     def downcase_email
       self.email = email.downcase
-    end
-
-    # 有効化トークンとダイジェストを作成および代入する
-    def create_activation_digest
-      self.activation_token = User.new_token
-      self.activation_digest = User.digest(activation_token)
     end
 
 end
