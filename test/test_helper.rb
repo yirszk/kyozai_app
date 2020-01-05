1b0b7479f76b2b13ab49105d2e1e4b2b9400dfe1
Yoichiro Suzuki <yir.szk@gmail.com>
2020-01-05 15:12:01 +0900

    Finish user edit, update, index, and destroy actions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,11 +7,18 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  include ApplicationHelper
 
   #テストユーザーがログイン中の場合にtrueを返す
   def is_logged_in?
     !session[:user_id].nil?
   end
 
-  # Add more helper methods to be used by all tests here...
+  # テストユーザーとしてログインする
+  # tutorial通りにやっても上手く行かないため、以下を参考に修正
+  # https://stackoverflow.com/questions/44461101/accessing-session-in-integration-test
+  def log_in_as(user, password: 'password')
+    post login_path, params: { session: { email: user.email, password: password} }
+  end
+
 end
