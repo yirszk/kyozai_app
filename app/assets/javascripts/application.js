b425595bb34cc94edb258b792ee8e50cb2b72cf0
Yoichiro Suzuki <yir.szk@gmail.com>
2020-01-10 22:35:55 +0900

    Remove some dead code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,25 +13,3 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
-
-(function(){
-    'use strict';
-    
-    var show = document.getElementById('show');
-    //#showを取得
-    var hide = document.getElementById('hide');
-    //#hideを取得
-  
-    show.addEventListener('click',function(){
-      //showをクリックした時、、
-      document.body.className = 'menu-open';
-      //bodyに.menu-openをつける
-    });
-  
-      hide.addEventListener('click',function(){
-      //hideをクリックした時、、
-      document.body.className = '';
-      //bodyのクラスを外す
-    });
-  
-})();
