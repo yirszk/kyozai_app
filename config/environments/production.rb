d5344b178d692bb0389c20444e362a1573e9159f
Yoichiro Suzuki <yir.szk@gmail.com>
2020-01-11 21:11:12 +0900

    fix some codes for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,8 +72,8 @@ Rails.application.configure do
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
       :authentication => :plain,
-      :user_name      => ENV['app157592224@heroku.com'],
-      :password       => ENV['vwwzv2oe1947'],
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
       :domain         => 'heroku.com',
       :enable_starttls_auto => true
   }
